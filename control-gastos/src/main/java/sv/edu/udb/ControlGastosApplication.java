f8de8fdfca8797bd99d951ade76afd40009352b0
Rodrigo Adrián Flores Pérez <138621494+rodriou4187@users.noreply.github.com>
2026-07-30 22:50:36 -0600

    Inserción de categorías de prueba iniciales
    Añadir datos de prueba iniciales para las categorías en la aplicación.

--- control-gastos/src/main/java/sv/edu/udb/ControlGastosApplication.java
+++ control-gastos/src/main/java/sv/edu/udb/ControlGastosApplication.java
@@ -1,7 +1,11 @@
 package sv.edu.udb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import sv.edu.udb.model.Categoria;
+import sv.edu.udb.repository.CategoriaRepository;
 
 @SpringBootApplication
 public class ControlGastosApplication {
@@ -10,4 +14,25 @@ public class ControlGastosApplication {
 		SpringApplication.run(ControlGastosApplication.class, args);
 	}
 
+	// Datos de prueba iniciales
+	@Bean
+	CommandLineRunner initData(CategoriaRepository categoriaRepository) {
+		return args -> {
+			if (categoriaRepository.count() == 0) {
+				categoriaRepository.save(Categoria.builder()
+						.nombre("Salario")
+						.tipo("INGRESO")
+						.build());
+				categoriaRepository.save(Categoria.builder()
+						.nombre("Comida")
+						.tipo("GASTO")
+						.build());
+				categoriaRepository.save(Categoria.builder()
+						.nombre("Transporte")
+						.tipo("GASTO")
+						.build());
+				System.out.println("Categorías de prueba insertadas");
+			}
+		};
+	}
 }
