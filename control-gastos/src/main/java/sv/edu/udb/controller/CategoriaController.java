4fe160a2e4ce269245b5d5fc79c2f9d30eec7789
Rodrigo Adrián Flores Pérez <138621494+rodriou4187@users.noreply.github.com>
2026-07-30 20:33:17 -0600

    Implementando CategoriaController con métodos CRUD

--- control-gastos/src/main/java/sv/edu/udb/controller/CategoriaController.java
+++ control-gastos/src/main/java/sv/edu/udb/controller/CategoriaController.java
@@ -1,4 +1,30 @@
 package sv.edu.udb.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sv.edu.udb.model.Categoria;
+import sv.edu.udb.service.CategoriaService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+@RequiredArgsConstructor
 public class CategoriaController {
+
+    private final CategoriaService categoriaService;
+
+    @PostMapping
+    public ResponseEntity<Categoria> crearCategoria(@RequestBody Categoria categoria) {
+        Categoria nueva = categoriaService.crearCategoria(categoria);
+        return new ResponseEntity<>(nueva, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> listarCategorias() {
+        List<Categoria> categorias = categoriaService.listarCategorias();
+        return ResponseEntity.ok(categorias);
+    }
 }
